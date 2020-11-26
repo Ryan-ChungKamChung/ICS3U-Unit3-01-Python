3d1e0a3af6c4cedcd57f88b9ed22e01f23963ff6
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-11-26 23:59:52 +0000

    minor fixes

--- adding.py
+++ adding.py
@@ -6,18 +6,19 @@
 
 
 def main():
-    # This function calculates the cost of a pizza
+    # This function finds the sum of 2 numbers
 
     # Input
+    print("This program adds 2 numbers together!")
     first_number = int(input("Please enter the first number: "))
     second_number = int(input("Please enter the second number: "))
-    print("This program adds 2 numbers together!")
-    
+
     # Process
     sum = first_number + second_number
 
     # Output
     print("{0}+{1}={2}".format(first_number, second_number, sum))
+
 
 if __name__ == "__main__":
     main()
